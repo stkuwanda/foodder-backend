a6a7a01045662dbb1783ca034d45376d7d161967
Simbarashe Kuwanda <stkuwanda@gmail.com>
2025-04-21 18:10:27 +0200

    removing debug logging

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -10,7 +10,6 @@ async function authenticate(req, res, next) {
 
   try {
     const decodedToken = verifyToken(token);
-    console.log('decodedToken:',decodedToken);
     req.body = req.body ? req.body : {};
     req.body.userId = decodedToken.id;
     next();
